e6cc32bb977cd62ecb681c6cf533d38351cc3cf1
prabhatkumar1379 <104418668+prabhatkumar1379@users.noreply.github.com>
2024-05-23 23:39:16 +0530

    Update nthsalary.sql

--- nthsalary.sql
+++ nthsalary.sql
@@ -1,7 +1,7 @@
 select * from IT_Employees
 
 --nth highset salary
-  
+  --
 select max(salary) from IT_Employees
 where Salary < (select max(salary) from IT_Employees
 where Salary < (select max(salary) from IT_Employees
